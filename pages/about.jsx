14611f38797e32bc15a93477c953bd0ba40fb7a1
Codesandmore <chackochanme@gmail.com>
2024-04-10 09:12:41 +0530

    About1

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,26 @@
+import React from 'react';
+import './about.css';
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div className="about-container">
+      <div className="about-section">
+        <h2>WHO ARE WE</h2>      
+      </div>
+      <div className="about-section">
+        <div className="about-text">
+          <h2>OUR MISSION</h2>
+          <p>Text about your mission...</p>
+        </div>
+      </div>
+      <div className="about-section">
+        <div className="about-text">
+          <h2>OUR VISION</h2>
+          <p>Text about your vision...</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
