e1bb3d4b3795ec6a2bb800443c733a2d94fc3937
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-04-18 17:52:01 +0530

    tests added

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,9 +1,17 @@
 import Gallery from "../components/gallery";
+import { awards } from "../Data/awards";
+import { notableEvents } from "../Data/notableEvents";
+import { recentEvents } from "../Data/recentEvents";
+import { majorEvents } from "../Data/majorEvents";
+import { voiceIt } from "../Data/voiceIt";
+import Card from "../components/card";
+import RecentEvents from "./Events/recentEvents";
 
 function Test() {
   return (
     <div>
       <Gallery />
+      <RecentEvents />
     </div>
   );
 }
