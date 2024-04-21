70e91ab996deb066fa04430dec2c8fd261058d5f
AKHIL-GIREESH <94656684+AKHIL-GIREESH@users.noreply.github.com>
2024-04-21 14:18:30 +0530

    Update awardsTop.jsx

--- pages/Awards/awardsTop.jsx
+++ pages/Awards/awardsTop.jsx
@@ -1,17 +1,43 @@
 const AwardTop = () => {
+  const pc = {
+    display: "flex",
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 0,
+  };
+
+  const mob = {};
   return (
     <>
-      <h1>WHAT ARE AWARDS?</h1>
+      <p
+        style={{
+          fontSize: window.innerHeight > window.innerWidth ? "2em" : "3.5em",
+          fontWeight: "800",
+        }}
+      >
+        WHAT ARE AWARDS?
+      </p>
+      <br />
       <div
         style={{
           display: "flex",
           width: "90%",
+          // border: "1px solid",
+          flexDirection:
+            window.innerHeight > window.innerWidth ? "column-reverse" : "row",
           justifyContent: "center",
           alignItems: "center",
           margin: 0,
         }}
       >
-        <div style={{ width: "40%" }}>
+        <div
+          style={
+            window.innerHeight < window.innerWidth
+              ? { width: "38%", fontSize: "1.2em", marginRight: "3vw" }
+              : { width: "100%" }
+          }
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit
           est leo, ac vulputate ante dignissim eget. Morbi sed nisi quis nunc
           pellentesque lobortis vitae id metus. Maecenas blandit libero sit amet
@@ -21,10 +47,14 @@ const AwardTop = () => {
         <img
           src="/images/aboutpic.jpg"
           alt="Who We Are"
-          style={{
-            width: "45%",
-            borderRadius: "10px",
-          }}
+          style={
+            window.innerHeight < window.innerWidth
+              ? {
+                  width: "45%",
+                  borderRadius: "10px",
+                }
+              : { width: "100%", borderRadius: "10px", marginBottom: "3vh" }
+          }
         />
       </div>
     </>
@@ -32,3 +62,4 @@ const AwardTop = () => {
 };
 
 export default AwardTop;
+
