22ca0a8c93de5f42febf6e358d3c1ee29377ac19
AKHIL-GIREESH <94656684+AKHIL-GIREESH@users.noreply.github.com>
2024-04-21 14:18:59 +0530

    Update awardBottom.jsx

--- pages/Awards/awardBottom.jsx
+++ pages/Awards/awardBottom.jsx
@@ -1,20 +1,38 @@
 import { awards } from "../../Data/awards";
 import Card from "../../components/card";
+import AwardsCard from "../../components/AwardsCard";
 
 const AwardBottom = () => {
-  const generateCards = () =>
-    awards.map((item) => (
-      <div key={item.id}>
-        <Card key={item.id} img={item.img} text={item.name} />
-        <p>{item.name}</p>
-        <p>{item.dec}</p>
-      </div>
+  const generateCards = () => {
+    return awards.map(({ img, dec, name, id }) => (
+      <AwardsCard img={img} name={name} dec={dec} flag={id % 2} />
     ));
+  };
+  // awards.map((item) => (
+  //   <div key={item.id} style={{ border: "1px solid" }}>
+  //     <Card key={item.id} img={item.img} text={item.name} />
+  //     <p>{item.name}</p>
+  //     <p>{item.dec}</p>
+  //   </div>
+  // ));
 
   return (
     <>
-      <h1>Our Awards</h1>
-      <div
+      <br />
+      <br />
+      <br />
+      <p
+        style={{
+          fontSize: window.innerHeight > window.innerWidth ? "2em" : "3.5em",
+          fontWeight: "800",
+        }}
+      >
+        OUR AWARDS
+      </p>
+      <br />
+      <div>{generateCards()}</div>
+
+      {/* <div
         style={{
           display: "flex",
           justifyContent:
@@ -23,11 +41,12 @@ const AwardBottom = () => {
               : "space-around",
           width: "100vw",
           alignItems: "center",
-          overflowX: "scroll",
+          overflowX:
+            window.innerHeight > window.innerWidth ? "scroll" : "hidden",
         }}
       >
         {generateCards()}
-      </div>
+      </div> */}
     </>
   );
 };
